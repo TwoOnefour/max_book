beff073573904366c4d0eb4dc8045e3506f5b07f
TwoOnefour <lys214412@gmail.com>
2023-05-08 07:20:26 +0800

    update

--- get_doc.py
+++ get_doc.py
@@ -1,8 +1,17 @@
+import os.path
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 import re
 import time
 import json
+path = os.path.split(os.path.realpath(__file__))[0] + "\\img"
+if os.path.exists(path):
+    print("已存在pdf，退出")
+    sys.exit()
+else:
+    os.mkdir(path)
 doc_url = "https://max.book118.com/html/2017/0122/86192397.shtm"  # 要爬的文档
 session = requests.session()
 session.headers.update({
@@ -12,14 +21,12 @@ html = session.get(doc_url).text
 
 soup = BeautifulSoup(html, "html.parser")
 
-soup.select("script")
-
 t = re.search(re.compile("(?:senddate:)(.*)"), str(soup.select("script")[5].next)).groups()[0][2:-2]
 view_token = re.search(re.compile("(?:view_token:)(.*)"), str(soup.select("script")[5].next)).groups()[0].strip("//预览的token")[2:-2]
 project_id = 1
 aid = doc_url.split("/")[-1].split(".")[0]
 actual_page = re.search(re.compile("(?:actual_page:)(.*)"), str(soup.select("script")[5].next)).groups()[0].split(",")[0].strip(" ")
-my_dict = {}
+doc_url_dict = {}
 for i in range(int(actual_page)):
     url_list = session.get("https://openapi.book118.com/getPreview.html", params={
         "t": t,
@@ -31,11 +38,11 @@ for i in range(int(actual_page)):
     time.sleep(1)
     now = json.loads(url_list.strip("jsonpReturn(").strip(")")[:-2])["data"]
     for j in now:
-        if my_dict.get(j) is None and now[j] != "":
-            my_dict[j] = now[j]
+        if doc_url_dict.get(j) is None and now[j] != "":
+            doc_url_dict[j] = now[j]
     # print(json.loads(url_list.strip("jsonpReturn(").strip(")")[:-2])["data"])
 # re.search(re.compile("(?:senddate:)(.*)") ,str(soup.select("script")[5].next)).groups() view_token
-print(my_dict)
-for i in my_dict:
-    with open(f"{i}.png", "wb") as f:
-        f.write(session.get(f"https:{my_dict[i]}").content)
+print(doc_url_dict)
+for i in doc_url_dict:
+    with open(f"{path}\\{i}.png", "wb") as f:
+        f.write(session.get(f"https:{doc_url_dict[i]}").content)
