1e38baaeb296dc7c90cfad630705bec654ad9bc4
TwoOnefour <lys214412@gmail.com>
2023-05-08 11:09:55 +0800

    update for encrypted doc

--- get_doc.py
+++ get_doc.py
@@ -25,11 +25,11 @@ class MaxDoc:
         })
         html = session.get(doc_url).text
         soup = BeautifulSoup(html, "html.parser")
-        t = re.search(re.compile("(?:senddate:)(.*)"), str(soup.select("script")[5].next)).groups()[0][2:-2]
-        view_token = re.search(re.compile("(?:view_token:)(.*)"), str(soup.select("script")[5].next)).groups()[0].strip("//预览的token")[2:-2]
+        t = re.search(re.compile("(?:senddate:)(.*),"), str(soup.select("script")[5].next)).groups()[0].replace("'", "").strip(" ")
+        view_token = re.search(re.compile("(?:view_token:)(.*)//预览的token"), str(soup.select("script")[5].next)).groups()[0].replace("'", "").strip(" ")
         project_id = 1
-        aid = doc_url.split("/")[-1].split(".")[0]
-        actual_page = re.search(re.compile("(?:actual_page:)(.*)"), str(soup.select("script")[5].next)).groups()[0].split(",")[0].strip(" ")
+        aid = re.findall(re.compile("(?:aid:)(.*),"), str(soup.select("script")[5].next))[-1].strip(" ")
+        actual_page = re.search(re.compile("(?:actual_page:)(.*),"), str(soup.select("script")[5].next)).groups()[0].strip(" ")
         doc_url_dict = {}
         for i in range(int(actual_page)):
             url_list = session.get("https://openapi.book118.com/getPreview.html", params={
@@ -40,10 +40,14 @@ class MaxDoc:
                 "page": i + 1
             }).text
             time.sleep(1)
-            now = json.loads(url_list.strip("jsonpReturn(").strip(")")[:-2])["data"]
-            for j in now:
-                if now[j] != "" and doc_url_dict.get(j) is None:
-                    doc_url_dict[j] = now[j]
+            now = json.loads(url_list.strip("jsonpReturn(").strip(")")[:-2])
+            if now["status"] != 200:
+                print("未知错误")
+                os.rmdir(path)
+                return
+            for j in now["data"]:
+                if now["data"][j] != "" and doc_url_dict.get(j) is None:
+                    doc_url_dict[j] = now["data"][j]
             # print(json.loads(url_list.strip("jsonpReturn(").strip(")")[:-2])["data"])
         # re.search(re.compile("(?:senddate:)(.*)") ,str(soup.select("script")[5].next)).groups() view_token
         # print(doc_url_dict)
@@ -54,5 +58,5 @@ class MaxDoc:
 
 
 if __name__ == "__main__":
-    get_doc = MaxDoc("https://max.book118.com/html/2017/0122/86192397.shtm")  # 可以把url换成你要爬的文档url
+    get_doc = MaxDoc(sys.argv[1])  # 可以把url换成你要爬的文档url
     get_doc.run()
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Description
+
+这是一个原创力文档的爬虫，输入网址，可以将文档下载到img文件夹下
+
+# Usage
+```angular2html
+python.exe get_doc.py 'maxbook的文档url'
+```
+
+**For example**
+```angular2html
+python.exe get_doc.py https://max.book118.com/html/2017/0122/86192397.shtm
+```
+
+![输出图片](https://bucket.pursuecode.cn/upload/2023/05/2.png)
+
+# Disclaimer
+此程序仅用于学习交流，禁止用于商业用途，如有认为本项目有侵权嫌疑，请联系我删除本项目
